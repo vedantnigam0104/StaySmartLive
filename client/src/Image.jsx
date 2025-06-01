018ad7b7c18fe2c92c933f06a2ce914376f524ca
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2025-06-01 15:18:40 +0530

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,14 +1,14 @@
 export default function Image({src, ...rest}) {
   // Check if src is not empty and does not already include 'http' or 'uploads'
   if (src && !src.includes('http') && !src.startsWith('uploads/')) {
-    src = 'https://stay-smart-live-dzqc.vercel.app/uploads/' + src;
+    src = 'https://staysmart-uxcc.onrender.com/uploads/' + src;
   }
   else
   {
-    src = 'https://stay-smart-live-dzqc.vercel.app/' + src;
+    src = 'https://staysmart-uxcc.onrender.com/' + src;
   }
 
-  //console.log('Image source:', 'https://stay-smart-live-dzqc.vercel.app/' + src); // Debugging line
+  //console.log('Image source:', 'https://staysmart-uxcc.onrender.com/' + src); // Debugging line
 
   return (
     <img {...rest} src={src} alt={'Image not available'} />
