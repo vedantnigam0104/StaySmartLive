0ba827d6066d8d54689b1ef600f45b91a44b979c
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2025-06-01 13:48:54 +0530

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,14 +1,14 @@
 export default function Image({src, ...rest}) {
   // Check if src is not empty and does not already include 'http' or 'uploads'
   if (src && !src.includes('http') && !src.startsWith('uploads/')) {
-    src = 'http://localhost:4000/uploads/' + src;
+    src = 'https://stay-smart-live-dzqc.vercel.app/uploads/' + src;
   }
   else
   {
-    src = 'http://localhost:4000/' + src;
+    src = 'https://stay-smart-live-dzqc.vercel.app/' + src;
   }
 
-  //console.log('Image source:', 'http://localhost:4000/' + src); // Debugging line
+  //console.log('Image source:', 'https://stay-smart-live-dzqc.vercel.app/' + src); // Debugging line
 
   return (
     <img {...rest} src={src} alt={'Image not available'} />
