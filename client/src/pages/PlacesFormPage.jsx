e72b2c72203775f2bfe17f4a6fef980a6cabecea
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2026-06-20 17:51:32 +0530

    Update PlacesFormPage.jsx

--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -34,7 +34,7 @@ export default function PlacesFormPage() {
     if (!id) {
       return;
     }
-    axios.get(`https://stay-smart-live.vercel.app/api/places/${id}`).then(response => {
+    axios.get(`/api/places/${id}`).then(response => {
       const { data } = response;
       setTitle(data.title);
       setAddress(data.address);
@@ -93,13 +93,13 @@ export default function PlacesFormPage() {
     };
     if (id) {
       // update
-      await axios.put('https://stay-smart-live.vercel.app/api/places', {
+      await axios.put('/api/places', {
         id, ...placeData
       });
       setRedirect(true);
     } else {
       // new place
-      await axios.post('https://stay-smart-live.vercel.app/api/places', placeData);
+      await axios.post('/api/places', placeData);
       setRedirect(true);
     }
   }
