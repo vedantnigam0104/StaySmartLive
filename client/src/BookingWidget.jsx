cc97b92185243f10d38efdeb05c0c4b1a9a9a365
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2026-06-20 17:54:44 +0530

    Update BookingWidget.jsx

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -35,7 +35,7 @@ export default function BookingWidget({ place }) {
     }
 
     try {
-      const response = await axios.post('https://stay-smart-live.vercel.app/api/bookings', {
+      const response = await axios.post('/api/bookings', {
         checkIn, checkOut, numberOfGuests, name, phone,
         place: place._id,
         price: numberOfNights * place.price,
