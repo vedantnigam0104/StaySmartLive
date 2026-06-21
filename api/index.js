f79504f6246780cb3e4014375943831ee5e553f5
VEDANT NIGAM <vedantnigam0104@gmail.com>
2026-06-21 15:06:46 +0530

    some new changes.

--- api/index.js
+++ api/index.js
@@ -47,13 +47,7 @@ app.use(cors({
 
 //const PaytmChecksum = require('./PaytmChecksum');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+
 
 async function sendOtpEmail(to, otp) {
   try {
