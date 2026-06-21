d92eae67d566400412b521881d04f1f232a4bb91
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2026-06-21 12:47:58 +0530

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -5,9 +5,9 @@ export default function Image({ src, ...rest }) {
 
   // If it's a relative image uploaded locally (not a full URL)
   if (!src.includes('http') && !src.startsWith('uploads/')) {
-    src = 'https://stay-smart-live.vercel.app/uploads/' + src;
+    src = 'https://staysmartlive.onrender.com/uploads/' + src;
   } else if (src.startsWith('uploads/')) {
-    src = 'https://stay-smart-live.vercel.app/' + src;
+    src = 'https://staysmartlive.onrender.com/' + src;
   }
   // Else, it's a full external URL – do nothing
 
